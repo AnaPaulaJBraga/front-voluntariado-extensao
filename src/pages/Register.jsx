892e1d1c6c24fb39e30a0213a77dfa11e35d6fb6
Ana Paula Junqueira <ana.paula.braga@accenture.com>
2026-04-23 18:34:47 -0300

    inicia projeto

---
+++ src/pages/Register.jsx
@@ -0,0 +1,16 @@
+const Register = () => {
+  return (
+    <div>
+      <h1>Cadastro</h1>
+
+      <form>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
