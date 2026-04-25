dbe0d4e79e74dbfd49366972bc3a4a11018db3d8
souzaedvaldo316-bit <souzaedvaldo316@gmail.com>
2026-04-25 01:58:45 -0300

    feat: alteracoes na tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,21 @@
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
 
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
-      </form>
+      <div style={{border: "1px solid #ccc", padding: "20px", borderRadius: "8px", width: "300px", boxShadow: "0 4px 10px rgba(0,0,0,0.1)"}}>
+
+      <h1 style={{ textAlign: "center"}}>Login</h1>
+            <form style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+
+              <label htmlFor="email">Email</label>
+              <input type="email" placeholder="Digite seu email"/>
+
+              <label htmlFor="senha">Senha</label>
+              <input type="password" placeholder="Digite sua senha"/>
+
+              <button type="submit">Entrar</button>
+            </form>
+      </div>
     </div>
   );
 };
